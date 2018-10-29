540ddea50ae829bd59c26cfe51f4b423cca927c0
Petru Guzun <pguzun@gmail.com>
2018-10-30 01:57:08 +0200

    took apache client as model.

---
+++ README.md
@@ -0,0 +1,6 @@
+# network-algorithms-ntp
+university/network-algorithms/ntp
+
+## In order to build you need to have installed
+
+  * Java 8
---
+++ client/src/main/java/ro/unitbv/pguzun/ntp/api/Mode.java
@@ -0,0 +1,15 @@
+package ro.unitbv.pguzun.ntp.api;
+
+public enum Mode {
+    MODE_RESERVED,
+    MODE_SYMMETRIC_ACTIVE,
+    MODE_SYMMETRIC_PASSIVE,
+    MODE_CLIENT ,
+    MODE_SERVER,
+    MODE_BROADCAST ,
+    MODE_CONTROL_MESSAGE,
+    MODE_PRIVATE;
+    
+    public static void main(String[] args) {
+    }
+}
